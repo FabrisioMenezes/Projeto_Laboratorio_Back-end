8a6782f35ecf7cb65a7bbfc18fbbb3d1281b6ba6
Khozex <ismaelteles75@gmail.com>
2021-09-15 00:52:25 -0300

    Implementacao do metodo POST

--- src/main/java/unit/grupo3/Biolab/controller/ResearcherController.java
+++ src/main/java/unit/grupo3/Biolab/controller/ResearcherController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,4 +32,12 @@ public class ResearcherController {
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
+
+    @PostMapping
+    ResponseEntity createResearcher(@RequestBody ResearcherEntity researcherEntity){
+        
+        repository.save(researcherEntity);
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
 }
