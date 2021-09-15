527bfe868815ac033e5e45a8b71fee497369f4d7
Artur Silva <artur.silva@ubots.com.br>
2021-09-14 22:18:18 -0300

    Criação do projeto

---
+++ src/test/java/unit/grupo3/Biolab/BiolabApplicationTests.java
@@ -0,0 +1,13 @@
+package unit.grupo3.Biolab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiolabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
