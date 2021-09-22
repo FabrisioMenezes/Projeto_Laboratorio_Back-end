65d5ea9c18619b072ab211fc70232b86bb4baeea
Khozex <ismaelteles75@gmail.com>
2021-09-22 05:53:43 -0300

    Habilitaçao do CORS

--- src/main/java/unit/grupo3/Biolab/controller/ResearcherController.java
+++ src/main/java/unit/grupo3/Biolab/controller/ResearcherController.java
@@ -3,6 +3,7 @@ package unit.grupo3.Biolab.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import unit.grupo3.Biolab.repository.ResearcherRepository;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/researchers")
 public class ResearcherController {
 
